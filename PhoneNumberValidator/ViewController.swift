facfc2dc199a710e188495ccf18f9ceec368a4e9
rajavijayakumarr <raja.nayagam@anywhere.co>
2019-10-30 18:38:39 +0530

    made phone number formatter app

--- PhoneNumberValidator/ViewController.swift
+++ PhoneNumberValidator/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import PhoneNumberKit
+
+class PhoneNumberFormatter {
+    
+    static var phoneNumber = PhoneNumberKit()
+    
+    static func getFormattedNumber(fromNumber number: String) throws -> String {
+        let phoneNumberObj = try phoneNumber.parse(number)
+        return phoneNumber.format(phoneNumberObj, toType: .e164)
+    }
+    
+}
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var formattedNumberTextField: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
-
+    @IBAction func formatButtonAction(_ sender: Any) {
+        
+        guard let text = phoneNumberTextField.text else {
+            formattedNumberTextField.text = "Invalid text in the phone number text field"
+            return
+        }
+        
+        guard let phoneNumber = try? PhoneNumberFormatter.getFormattedNumber(fromNumber: text) else {
+            formattedNumberTextField.text = "Invalid Phone number format"
+            return
+        }
+        
+        formattedNumberTextField.text = phoneNumber
+    }
+    
 }
 
